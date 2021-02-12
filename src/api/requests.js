3aeda29523eada8c92dfa7afe3baa7f49b043055
Your Name <aznaurovdato@gmail.com>
2021-02-12 16:16:24 -0500

    api refactoring

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = process.env.REACT_APP_API_KEY
+const API_KEY = 05c32981e48de1e61d5e174e4f052daa
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
