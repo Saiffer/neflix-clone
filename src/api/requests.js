746d2c1e5ee8c2a46bf6fe2a23f0b13da5a77f18
Your Name <aznaurovdato@gmail.com>
2021-02-12 16:22:56 -0500

    123

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = 05c32981e48de1e61d5e174e4f052daa
+const API_KEY = "05c32981e48de1e61d5e174e4f052daa";
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
@@ -8,8 +8,8 @@ const requests = {
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
-}
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
-export default requests
+export default requests;
 // https://api.themoviedb.org/3/discover/tv?api_key=###&with_networks=213
